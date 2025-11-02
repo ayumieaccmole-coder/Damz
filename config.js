1fdba16bb074137e4b741f407783beff35b604ba
ayumieaccmole-coder <ayumieaccmole@gmail.com>
2025-11-02 15:05:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TOKEN_BOT_XYAALZ: "8438508034:AAEIiiTllAL8ToGgihNenEcnjt9EDC1XNko",
+  OWNER_ID: "7870295166",
+};
+
+/*
+  Creator : DamzOfficial
+  Telegram : t.me/DamzOfficiall
+*/
